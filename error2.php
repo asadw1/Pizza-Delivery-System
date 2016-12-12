946674accfcf69318cc34988e3835081a0ee618d
Asad <asad.waheed.23@gmail.com>
2016-12-11 22:18:01 -0600

    Update error2.php

--- error2.php
+++ error2.php
@@ -1,4 +1,8 @@
 <?php
+
+// White-box configured test file
+// When changes are made to checkout.php, this file will generate any Parse or Syntax errors in the browser
+// Excellent test tool!
 ini_set("display_errors", "1");
 error_reporting(E_ALL);
 include 'checkout.php';
