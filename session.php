c75fd67d4f622fe9d3d772395a21ca4a37a782b4
Asad <asad.waheed.23@gmail.com>
2016-12-11 22:24:37 -0600

    Update session.php

--- session.php
+++ session.php
@@ -1,19 +1,21 @@
 <?php
 
-
-//conect to myphpadmin database
+//Using mysqli function to connect to DB
 @ $db = new mysqli('localhost', 'user_eg','13669618', 'Database_Project' );
 
-
+// Set current table and init session variables for the user
 $currenttb="UserDB";
 session_start();
 $user_check=$_SESSION['login_user'];
+
 
 $query="select sign_email from $currenttb where sign_email="."'$user_check'"; 
 	$result = $db->query($query);
 	$row = mysqli_fetch_row( $result );
 	$login_session =$row['sign_email'];
-	if(!isset($login_session)){
+
+// Check if the user's session failed, if so send them back to the homepage
+if(!isset($login_session)){
 $db->close(); // Closing Connection
 header('Location: index.html'); // Redirecting To Home Page
 }
